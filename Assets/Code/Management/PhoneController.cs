29a04dc4d3e9310268b98fd63783cf8a6ef1ec8d
Khevynn Sá <115984821+Khevynn@users.noreply.github.com>
2023-04-23 02:38:56 +0100

    Phone issue fix

--- Assets/Code/Management/PhoneController.cs
+++ Assets/Code/Management/PhoneController.cs
@@ -13,7 +13,7 @@ public class PhoneController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        phone.SetActive(false);
     }
 
     // Update is called once per frame
